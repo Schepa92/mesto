e8c71148dd2d6e291a45442930ded3f77abeafb2
Schepa <sschepankevich@yandex.ru>
2022-06-30 20:23:24 +0700

    add class card (not remove element)

---
+++ modules/card.js
@@ -0,0 +1,59 @@
+import { elementsBlock } from './index.js';
+
+export const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
+
+export class Card {
+  constructor(name, link) {
+    this._name = name;
+    this._link = link;
+  }
+
+  _getTemplate() {
+    const cardElement = document
+      .querySelector('.elements')
+      .content.cloneNode(true);
+    return cardElement;
+  }
+
+  generateCard() {
+    this._element = this._getTemplate();
+    this._element.querySelector('.element__image').src = this._link;
+    this._element.querySelector('.element__footer_title').textContent =
+      this._name;
+    this._element.addEventListener('click', (evt) => {
+      console.log(evt);
+    });
+    return this._element;
+  }
+}
+
+initialCards.forEach((item) => {
+  const card = new Card(item.name, item.link);
+  const cardElement = card.generateCard();
+  document.querySelector('.elements').append(cardElement);
+});
--- modules/index.js
+++ modules/index.js
@@ -1,33 +1,8 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-  },
-];
+import { initialCards, Card } from './card.js';
 
 /////////////////////////////////////////////////////////////////////////////////////
 
-const elementsBlock = document.querySelector('.elements');
+export const elementsBlock = document.querySelector('.elements');
 const profileButton = document.querySelector('.edit-button');
 const profilePopup = document.querySelector('.profile-popup');
 const addButton = document.querySelector('.add-button');
@@ -38,8 +13,8 @@ const picPopup = document.querySelector('.pic-popup');
 
 function showProfilePopup() {
   profilePopup.classList.add('popup_show');
-  authorTitleInput.value = authorTitle.textContent;
-  authorSubtitleInput.value = authorSubtitle.textContent;
+  author.value = authorTitle.textContent;
+  aboutAuthor.value = aboutAuthorSubtitle.textContent;
 }
 
 function showAddPopup() {
@@ -85,121 +60,63 @@ document.addEventListener('click', (evt) => {
 
 /////////////////////////////////////////////////////////////////////////////////////
 // ЗАСТАВЛЯЕМ РАБОТАТЬ ОКНО РЕДАКТИРОВАНИЯ ПРОФИЛЯ
-
-const submitProfileButton = profilePopup.querySelector(
-  '.profile-popup__window_submit'
-);
+const profilePopupForm = document.forms.profilePopup__form;
+const author = profilePopupForm.elements.author;
+const aboutAuthor = profilePopupForm.elements.aboutAuthor;
+const saveSubmit = profilePopupForm.elements.save;
 
 const authorTitle = document.querySelector('.profile__info_author-text');
-const authorTitleInput = profilePopup.querySelector(
-  '.profile-popup__window_form_author'
-);
-const authorSubtitle = document.querySelector('.profile__info_about-author');
-const authorSubtitleInput = document.querySelector(
-  '.profile-popup__window_form_about-author'
+const aboutAuthorSubtitle = document.querySelector(
+  '.profile__info_about-author'
 );
 
 function setAuthorInfo() {
-  authorTitle.textContent = authorTitleInput.value;
-  authorSubtitle.textContent = authorSubtitleInput.value;
+  authorTitle.textContent = author.value;
+  aboutAuthorSubtitle.textContent = aboutAuthor.value;
+  author.value = '';
+  aboutAuthor.value = '';
   closePopup();
-  authorTitleInput.value = '';
-  authorSubtitleInput.value = '';
 }
 
-submitProfileButton.addEventListener('click', setAuthorInfo);
+saveSubmit.addEventListener('click', setAuthorInfo);
 
 /////////////////////////////////////////////////////////////////////////////////////
 // ЗАСТАВЛЯЕМ РАБОТАТЬ ОКНО ДОБАВЛЕНИЯ ПУБЛИКАЦИИ
 
-const submitAddButton = document.querySelector('.add-popup__window_submit');
-const elementTitleInput = document.querySelector(
-  '.add-popup__window_form-title'
-);
-const elementLinkInput = document.querySelector('.add-popup__window_form-link');
+const addPopupForm = document.forms.addPopup__form;
+const title = addPopupForm.elements.title;
+const link = addPopupForm.elements.link;
+const addSubmit = addPopupForm.elements.add;
 
 function addItemElement() {
-  const addedArrayElement = {
-    name: `${elementTitleInput.value}`,
-    link: `${elementLinkInput.value}`,
-  };
-  if (
-    elementTitleInput.value.length !== 0 &&
-    elementLinkInput.value.length !== 0
-  ) {
-    initialCards.unshift(addedArrayElement);
-    initialCardsElement.unshift('');
-    console.log(initialCardsElement);
-    console.log(initialCards);
-    elementTitleInput.value = '';
-    elementLinkInput.value = '';
+  if (title.value.length !== 0 && link.value.length !== 0) {
+    const card = new Card(`${title.value}`, `${link.value}`);
+    const cardElement = card.generateCard();
+    document.querySelector('.elements').prepend(cardElement);
+    title.value = '';
+    link.value = '';
     closePopup();
-    createItemElement(0);
-
-    console.log(initialCardsElement);
   } else {
     alert('Введите информацию!');
   }
 }
 
-submitAddButton.addEventListener('click', addItemElement);
-
-/////////////////////////////////////////////////////////////////////////////////////
-// ФУНКЦИЯ СОЗДАНИЯ ЭЛЕМЕНТА
-
-const initialCardsElement = [];
-
-function createItemElement(index) {
-  const nameImage = initialCards[index].name;
-  const linkImage = initialCards[index].link;
-  const itemElement = document.createElement('div');
-  itemElement.classList.add('element');
-
-  const elementImage = document.createElement('img');
-  elementImage.classList.add('element__image');
-  elementImage.setAttribute('src', `${linkImage}`);
-
-  const elementTrash = document.createElement('img');
-  elementTrash.classList.add('element__trash-icon');
-  elementTrash.setAttribute('src', './images/trash.svg');
-
-  const elementFooter = document.createElement('div');
-  elementFooter.classList.add('element__footer');
-
-  const elementFooterTitle = document.createElement('h4');
-  elementFooterTitle.classList.add('element__footer_title');
-  elementFooterTitle.textContent = nameImage;
-
-  const elementFooterLike = document.createElement('img');
-  elementFooterLike.setAttribute('src', './images/like.svg');
-  elementFooterLike.classList.add('element__footer_like');
-
-  elementFooter.append(elementFooterTitle, elementFooterLike);
-  itemElement.append(elementImage, elementTrash, elementFooter);
-  elementsBlock.prepend(itemElement);
-  initialCardsElement[index] = itemElement;
-}
-
-function addElementStartPage() {
-  for (let i = 0; i <= initialCards.length; i++) {
-    createItemElement(i);
-
-    elementsBlock.append(initialCardsElement[i]);
-  }
-}
+addSubmit.addEventListener('click', addItemElement);
 
 /////////////////////////////////////////////////////////////////////////////////////
 // ФУНКЦИЯ УДАЛЕНИЯ ЭЛЕМЕНТА
 
-function deleteItemElement() {}
-
 elementsBlock.addEventListener('click', (evt) => {
   if (evt.target.classList.contains('element__trash-icon')) {
+    console.log(evt.target);
     const index = [
       ...elementsBlock.querySelectorAll('.element__trash-icon'),
     ].indexOf(evt.target);
-    initialCardsElement[index].remove();
-    initialCardsElement.splice(index, 1);
+    initialCards.splice(index, 1);
+
+    // let del = document.querySelector(`elements div:nth-child(${index})`);
+
+    console.log(evt.currentTarget.querySelectorAll('element'));
   }
 });
 
@@ -213,6 +130,3 @@ elementsBlock.addEventListener('click', (evt) => {
 });
 
 /////////////////////////////////////////////////////////////////////////////////////
-
-// ВЫЗОВ ФУНКЦИИ НАЧАЛЬНОЙ ОТРИСОВКИ ЭЛЕМЕНТОВ
-addElementStartPage();
